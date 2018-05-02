b492355519d0c86a68e692df019019b04cf10eae
cocosev <cristinaseva4@gmail.com>
2018-05-02 12:52:25 +0200

    ji

--- imagetoarray.py
+++ imagetoarray.py
@@ -1,16 +1,20 @@
 from PIL import Image
 import numpy as np
-
+import matplotlib.pyplot as plt 
 
 # Nombre del archivo a leer
-filename = "pera.png"
-# Tamaño final imagen
+filename = "perita.png"
+# Tamanho final imagen
 size = [20, 20]
 
 # Convierte la imagen a blanco y negro (no escala grises)
 im = Image.open(filename).convert('1', dither=Image.NONE)
-# Cambia el tamaño
+# Cambia el tamanho
 im = im.resize(size, Image.ANTIALIAS)
 # Pasa a array de numpy
-figure_array = np.logical_not(np.array(im))
 
+figure_array = np.logical_not(im)
+
+plt.imshow(figure_array)
+plt.show()
+print(type(im))
